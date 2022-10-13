355498403f753151d62017d29dbc2bba4cc8860d
Vinicius Becaleti <vinicius@becaleti.com>
2022-10-13 07:42:29 -0300

    Removed folder storybook-static

--- storybook-static/assets/preview.adf105a4.js
+++
@@ -1,2 +0,0 @@
-import{t as o}from"./index.1064fc90.js";import"./iframe.5e6307a0.js";import"./es.map.constructor.592d2a9a.js";import"./es.number.to-fixed.6c08d3d1.js";import"./index.932e5d62.js";const s={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{theme:o.dark}};export{s as parameters};
-//# sourceMappingURL=preview.adf105a4.js.map
